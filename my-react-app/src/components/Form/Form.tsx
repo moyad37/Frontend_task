2fa2cfc7ef9ab21b00686551da3abfc84593a781
jkrahofer <jk@umdaschgroup-ventures.com>
2024-05-08 11:01:47 +0200

    temporary fix of form validation

--- my-react-app/src/components/Form/Form.tsx
+++ my-react-app/src/components/Form/Form.tsx
@@ -1,171 +1,67 @@
-import PhoneInput from "react-phone-number-input/input";
 import { useForm } from "react-hook-form";
 import "react-phone-number-input/style.css";
-/* import CountryList from "./CountryList"; */
-import { useState } from "react";
 import clsx from "clsx";
 import Input from "./Input";
-import { z, ZodError } from "zod";
-import { E164Number } from "libphonenumber-js";
-import { Value } from "react-phone-number-input/core";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  firstName: z.string().min(1),
-  lastName: z.string().min(1),
-  title: z.string().optional(),
-  street: z.string().optional(),
-  city: z.string().optional(),
-  zip: z.number().optional(),
-  country: z.string().optional(),
-  phoneNumber: z.string().optional(),
+  firstName: z.string().min(1, 'You need to write at least one char!'),
+  lastName: z.string().min(1, 'isRequired'),
 });
 
-/* interface FormData {
-  firstName: string;
-  lastName: string;
-  title?: string;
-  street?: string;
-  city?: string;
-  zip: string;
-  country: string;
-  phoneNumber?: string;
-} */
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const [check, setCheck] = useState(false);
-  const [checkError, setCheckError] = useState(false);
-  const [scale, setScale] = useState(false);
 
   const {
     register,
     handleSubmit,
-    formState: { isValid, isDirty },
-    setValue,
-    getValues,
-  } = useForm<FormData>();
+    formState: { isValid, isDirty, errors },
+  } = useForm<FormData>({
+        resolver: zodResolver(schema),
+    mode: "onChange",
+      }
+  );
 
   const onSubmit = (data: FormData) => {
-    try {
-      schema.parse(data);
-      if (isValid) {
-        setCheck(true);
-        setScale(true);
-        setTimeout(() => {
-          setScale(false);
-        }, 100);
-
-        setTimeout(() => {
-          setCheck(false);
-        }, 3500);
-      }
-      console.log("Form data:", data);
-    } catch (error) {
-      if (error instanceof ZodError) {
-        setCheckError(true);
-        setTimeout(() => {
-          setCheckError(false);
-        }, 3500);
-        console.error("Validation errors:", error.errors);
-      } else {
-        throw error;
-      }
-    }
+    console.log(data);
   };
-  const scaleVisibility = scale ? "scale-0" : "";
+
+  console.log(isDirty, isValid, errors);
 
   return (
-    <form
-      className="border-2 rounded-lg p-3 flex flex-row flex-wrap bg-slate-50"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <Input
-        register={register}
-        type="text"
-        placeholder="Your first name"
-        name="firstName"
-        onBlur={register("firstName").onBlur}
-      />
-
-      <Input
-        register={register}
-        type="text"
-        placeholder="Your last name"
-        name="lastName"
-        onBlur={register("lastName").onBlur}
-      />
-
-      <Input register={register} type="text" name="Title" />
-
-      <Input
-        register={register}
-        type="text"
-        name="Street"
-        className="md:basis-1/4"
-      />
-
-      <Input
-        register={register}
-        type="text"
-        name="City"
-        className="md:basis-1/4"
-      />
-
-      <Input
-        register={register}
-        type="number"
-        name="ZIP"
-        className="md:basis-1/4"
-      />
-
-      <Input register={register} name="Country" CountryListBool />
-
-      <div className="form-group m-5 p-3 flex flex-col items-start basis-full md:basis-2/5">
-        <label className="my-2 text-xl font-bold" htmlFor="phoneNumber">
-          Phone Number
-        </label>
-        <PhoneInput
-          className="px-2 w-full border-2 rounded-lg  hover:shadow-lg focus:outline-none focus:ring focus:ring-violet-300 min-h-12 text-violet-300 focus:border-neutral-300"
-          placeholder="Enter phone number"
-          value={getValues("phoneNumber") || ""}
-          onChange={(value: E164Number | undefined) => {
-            setValue("phoneNumber", value?.toString() || "");
-          }}
+      <form
+          className="border-2 rounded-lg p-3 flex flex-row flex-wrap bg-slate-50"
+          onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+            placeholder="Your first name"
+            error={errors.firstName?.message}
+            {...register('firstName')}
+            required
         />
-      </div>
 
-      <div className="form-group m-5 p-3 flex flex-row justify-end items-center basis-full relative">
-        <button
-          className={clsx(
-            "p-4 rounded-xl text-white font-bold items-end max-h-16 max-w-40 min-w-32 basis-full",
-            isValid ? "bg-violet-500 hover:bg-violet-400" : "bg-gray-200"
-          )}
-          type="submit"
-          disabled={!isValid || !isDirty}
-        >
-          Submit
-        </button>
-        {isValid && check && (
-          <p
-            className={clsx(
-              "bg-green-300 p-3 absolute -top-10 text-white rounded-lg my-auto duration-500",
-              scaleVisibility
-            )}
+        <Input
+            placeholder="Your first name"
+            error={errors.lastName?.message}
+            {...register('lastName')}
+            required
+        />
+
+        <div className="form-group m-5 p-3 flex flex-row justify-end items-center basis-full relative">
+          <button
+              className={clsx(
+                  "p-4 rounded-xl text-white font-bold items-end max-h-16 max-w-40 min-w-32 basis-full",
+                  isValid && isDirty ? "bg-violet-500 hover:bg-violet-400" : "bg-gray-200"
+              )}
+              type="submit"
+              disabled={!isValid || !isDirty}
           >
-            Submit successful
-          </p>
-        )}
-        {checkError && (
-          <p
-            className={clsx(
-              "bg-red-300 p-3 absolute -top-10 text-white rounded-lg my-auto duration-500",
-              scaleVisibility
-            )}
-          >
-            Submit faild
-          </p>
-        )}
-      </div>
-    </form>
+            Submit
+          </button>
+        </div>
+      </form>
   );
 };
 
--- my-react-app/src/components/Form/Input.tsx
+++ my-react-app/src/components/Form/Input.tsx
@@ -1,94 +1,15 @@
 import clsx from "clsx";
-import { forwardRef, LegacyRef } from "react";
-import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
-import CountryList from "./CountryList";
+import {ComponentPropsWithoutRef, forwardRef} from "react";
 
-interface Props {
-  type: string;
-  placeholder: string;
-  name: string;
-  register: UseFormRegister<FieldValues>;
-  className?: string;
-  CountryListBool?: boolean;
-  errors?: FieldErrors<FormData>;
-}
-const Input = forwardRef<HTMLInputElement, Props>(
-  (
-    { type, placeholder, name, register, className, CountryListBool, errors },
-    ref
-  ) => {
-    return (
-      <div
-        className={clsx(
-          "form-group m-5 p-3 flex flex-col items-start basis-full",
-          className
-        )}
-      >
-        <label className="my-2 text-xl font-bold" htmlFor={name}>
-          {name}
-        </label>
-        {CountryListBool && <CountryList {...register(name)} />}
-        {!CountryListBool && (
-          <input
-            type={type}
-            placeholder={placeholder}
-            className="px-2 w-full border-2 rounded-lg  hover:shadow-lg focus:outline-none focus:ring focus:ring-violet-300 min-h-12 text-violet-300"
-            ref={ref as LegacyRef<HTMLInputElement>}
-            {...register(name)}
-          />
-        )}
-        {errors && errors[name as keyof FormData] && (
-          <span className="p-2 text-white bg-red-400 my-1 rounded-md">
-            {name} is required
-          </span>
-        )}
-      </div>
-    );
-  }
-);
-
-export default Input;
-
-/* 
-import clsx from "clsx";
-import { forwardRef, LegacyRef } from "react";
-import {
-  FieldErrors,
-  FieldValues,
-  UseFormRegister,
-  UseFormReturn,
-} from "react-hook-form";
-import CountryList from "./CountryList";
-
-interface Props {
-  type: string;
-  placeholder: string;
-  name: string;
-  errors?: FieldErrors<FormData>;
-  register: UseFormRegister<FieldValues>;
+interface Props extends ComponentPropsWithoutRef<'input'> {
+  error?: string | boolean;
   required?: boolean;
-  className?: string;
-  CountryListBool?: boolean;
-  formMethods: UseFormReturn<FieldValues>;
 }
-
 const Input = forwardRef<HTMLInputElement, Props>(
   (
-    {
-      type,
-      placeholder,
-      name,
-      errors,
-      register,
-      required,
-      className,
-      CountryListBool,
-      formMethods,
-    },
+    { name, className, error, required, ...rest },
     ref
   ) => {
-    const { getInputProps } = formMethods;
-
     return (
       <div
         className={clsx(
@@ -99,19 +20,16 @@ const Input = forwardRef<HTMLInputElement, Props>(
         <label className="my-2 text-xl font-bold" htmlFor={name}>
           {name}
         </label>
-        {CountryListBool && <CountryList {...getInputProps(name)} />}
-        {!CountryListBool && (
-          <input
-            type={type}
-            placeholder={placeholder}
+        <input
+            {...rest}
+            name={name}
+            required={required}
+            ref={ref}
             className="px-2 w-full border-2 rounded-lg  hover:shadow-lg focus:outline-none focus:ring focus:ring-violet-300 min-h-12 text-violet-300"
-            ref={ref as LegacyRef<HTMLInputElement>}
-            {...getInputProps(name, { required: required })}
-          />
-        )}
-        {errors && errors[name as keyof FormData] && (
+        />
+        {error && (
           <span className="p-2 text-white bg-red-400 my-1 rounded-md">
-            {name} is required
+            {error}
           </span>
         )}
       </div>
@@ -120,4 +38,3 @@ const Input = forwardRef<HTMLInputElement, Props>(
 );
 
 export default Input;
- */
